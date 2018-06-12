b4bc5860f76df828aba2dc86202a0236af20779f
Ryan <jragard@gmail.com>
2018-06-12 09:59:54 -0400

    fixed app so that routes are in app.jsx instead of index.js

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,102 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoList from './todoList.jsx';
-import { Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+import list from './todos.json';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: list, text: ""
+    }
+  }
+
+  // loops through the todos array, if the checkbox target's id matches a particular element's id, this toggles/updates the "completed" state
+
+  toggleTodo = (e) => {
+    const { todos } = this.state
+    
+    for (let i = 0; i < todos.length; i++) {
+        // eslint-disable-next-line
+        if (e.target.id == todos[i].id) {
+            todos[i].completed = !todos[i].completed;
+            this.setState( {
+                "completed": todos[i].completed
+            })
+        }
+    }
+ }
+
+// removes item from the todo List when user clicks the red X button
+
+  removeItem = (e) => {
+    const { todos } = this.state
+    let removedArray = [];
+
+    for (let i = 0; i < todos.length; i++) {
+        // eslint-disable-next-line 
+        if (e.target.id != todos[i].id) {
+            removedArray.push(todos[i])
+        }
+  }
+    this.setState(
+      {
+          todos: removedArray
+      }
+   )
+ }
+
+// removes all todo Items marked as complete when user clicks the "clear completed" button
+
+  removeAll = (e) => {
+    const { todos } = this.state;
+    let completedItems = todos.filter(todo => !todo.completed)
+    this.setState(
+      {
+        todos: completedItems
+      }
+    )
+ }
+
+// changes the "text" state to whatever the user inputs into the field
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value })
+  }
+
+// handles the submit event when user presses enter.  updates state to include user's todo item
+
+  handleSubmit = (e) => {
+
+    const { todos, text} = this.state;
+    e.preventDefault();
+
+    if (!text.length) {
+      return;
+    }
+    
+    this.setState(prevState => (
+      {
+      todos: [...prevState.todos, {"userId": 1,
+                                   "id": todos.length + 1,
+                                   "title": text,
+                                   "completed": false }],
+      text: ""
+      })
+    );
+      
+      let inputField = document.getElementById("input");
+      inputField.value = "";
+ }
   
 render() { 
-    const { todos, toggleTodo, removeItem, removeAll, handleChange, handleSubmit } = this.props;
-    
+    const { todos, text } = this.state;
+    const { toggleTodo, removeItem, removeAll, handleChange, handleSubmit } = this;
+
+    const ActiveList = todos.filter(todo => !todo.completed);
+    const CompletedList = todos.filter(todo => todo.completed);
+
     return (
       <React.Fragment>
 
@@ -27,7 +116,19 @@ render() {
           </header>
 
           <section className="main">
-            <TodoList todos={todos} toggleTodo={toggleTodo} removeItem={removeItem}/>
+            <Switch>
+            <Route 
+            exact path="/"
+            render={props => <TodoList {...props} todos={todos} text={text} toggleTodo={toggleTodo} removeItem={removeItem} removeAll={removeAll} handleChange={handleChange} handleSubmit={handleSubmit} /> } />
+            <Route
+            path="/active"
+            render={props => <TodoList {...props} todos={ActiveList} text={text} toggleTodo={toggleTodo} removeItem={removeItem} removeAll={removeAll} handleChange={handleChange} handleSubmit={handleSubmit} /> } />
+            <Route
+            path="/completed"
+            render={props => <TodoList {...props} todos={CompletedList} text={text} toggleTodo={toggleTodo} removeItem={removeItem} removeAll={removeAll} handleChange={handleChange} handleSubmit={handleSubmit} />} />
+
+            </Switch>
+            {/* <TodoList todos={todos} toggleTodo={toggleTodo} removeItem={removeItem}/> */}
           </section>
 
           <footer className="footer">
@@ -56,4 +157,6 @@ render() {
     );
   };
 };
+
+
 
